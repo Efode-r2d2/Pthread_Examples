eeb7df649a8251167ddd062151844fb71aca8e4b
EFRIEM-DESALEW <efidesalew@gmail.com>
2018-07-27 14:42:45 +0300

    first commit

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+
+/*Global variable accessible to all threads */
+
+int thread_count;
+
+/* thread function */
+void * Hello(void* rank);
+
+int main(){
+
+}
