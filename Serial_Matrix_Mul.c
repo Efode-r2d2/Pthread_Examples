9360587a0847fa303ea5f55906705a876e170683
EFRIEM-DESALEW <efidesalew@gmail.com>
2018-07-28 14:16:21 +0300

    serial matrix multiplication program using c

--- Serial_Matrix_Mul.c
+++ Serial_Matrix_Mul.c
@@ -6,26 +6,42 @@
 /* initializing one dimensional array */
 void init(int * array, int size);
 /* initializing two dimensional array */
-void init(int * array, int col, int row);
+void init2D(int ** array, int col, int row);
 /* printing one dimensional array */
 void print(int * array, int size);
 /* printing two dimensional array */
-void print(int* array, int col, int row);
+void print2D(int** array, int col, int row);
 
 int main(void)
 {
 	
 	return 0;
 }
-/* initialization function */
+/* initializing one dimensional array */
 void init(int * array, int size){
 	srand(1);
 	for(int i=0;i<size;i++){
 		array[i] = rand();	
 	}
 }
-/* a function to print an array */
+/* printing one dimensional array */
 void print(int * array, int size){
-	for(int i=0;i<
+	for(int i=0;i<size;i++){
+		printf("%d \n",array[i]);
+	}
 }
+/* initializing two dimensional array */
+void init2D(int ** array, int col, int row){
+	srand(1);
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			array[i][j]= rand();
+		}	
+	}
+}
+/* printing two dimensional array */
+void print2D(int** array, int col, int row){
+	
+}
+
 
